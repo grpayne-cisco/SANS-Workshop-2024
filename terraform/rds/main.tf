eb2fa21667155b48354547a2b4daf5ce4a437644
Thomas Tusia <ttusia@cisco.com>
2024-09-15 13:36:41 -0600

    updating terraform

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -4,11 +4,11 @@ resource "aws_db_instance" "my_db" {
   engine_version    = "5.7"
   instance_class    = "db.t3.micro"
   username          = "root"
-  password          = "my_super_secret_password"
-  #manage_master_user_password = true
+  #password          = "my_super_secret_password"
+  manage_master_user_password = true
   storage_encrypted = false
-  #tags = {
-  #  data_classification = "public"
-  #  owner_email = "admin@mydomain.com"
-  #}
+  tags = {
+    data_classification = "public"
+    owner_email = "admin@mydomain.com"
+  }
 }
